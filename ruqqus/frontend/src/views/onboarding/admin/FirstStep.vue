5ea0e71def2d49a5d310c5a4e97ac20b6688da69
kek7198 <grayboy99@protonmail.com>
2021-08-16 15:53:34 -0500

    onboarding step

--- ruqqus/frontend/src/views/onboarding/admin/FirstStep.vue
+++ ruqqus/frontend/src/views/onboarding/admin/FirstStep.vue
@@ -1,12 +1,138 @@
 <template>
 	<div class="flex flex-col flex-grow h-full">
-		<div class="bg-white border shadow-xs rounded overflow-hidden">
-			<h1>
+		<div class="flex flex-col items-center">
+			<h1 class="h2">
 				Welcome to Ruqqus
 			</h1>
-			<p class="mt-1">
+			<p class="mt-1 text-gray-600">
 				Glad to have you here. Let's get your community rolling.
 			</p>
+			         <div class="mt-6 w-full text-left overflow-hidden">
+            <div class="h-full relative bg-white shadow-xl rounded-lg overflow-hidden">
+              <div class="flex items-center justify-center w-full h-14 bg-gradient-to-t from-gray-200 to-gray-100 border-b px-4">
+                <div class="flex space-x-1.5 mr-4">
+                  <div class="w-3 h-3 bg-red-400 rounded-full border-purple-400"></div>
+                  <div class="w-3 h-3 bg-yellow-400 rounded-full border-yellow-400"></div>
+                  <div class="w-3 h-3 bg-green-400 rounded-full border-purple-400"></div>
+                </div>
+                <div class="w-full bg-gray-50 border border-gray-300 hover:border-gray-400 transition duration-200 px-2 py-1 rounded text-sm text-gray-500 hover:text-gray-600 text-center shadow-inner">
+                  https://www.ruqqus.com/+ruqqus
+                </div>
+              </div>
+              <div class="px-5 py-5">
+                <div class="grid grid-cols-8 gap-5">
+                  <div class="col-span-full p-5 rounded border-2 border-purple-400 bg-purple-100">
+                    <div class="flex items-center">
+                      <span class="w-24 h-3 bg-purple-200 rounded-full"></span>
+                      <div class="flex items-center space-x-3 ml-auto">
+                        <span class="w-8 h-3 bg-purple-200 rounded-full"></span>
+                        <span class="w-8 h-3 bg-purple-200 rounded-full"></span>
+                        <span class="w-8 h-3 bg-purple-200 rounded-full"></span>
+                        <div class="w-6 h-6 rounded bg-green-300"></div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="hidden md:block col-span-2 p-5 rounded border-2 border-purple-400 bg-purple-100">
+                    <ul>
+                      <li class="flex items-center text-purple-400 py-2">
+                        <span class="w-1/3 h-3 bg-purple-200 rounded-full"></span>
+                      </li>
+                      <li class="flex items-center text-purple-400 py-2">
+                        <span class="w-4/5 h-3 bg-purple-200 rounded-full"></span>
+                      </li>
+                      <li class="flex items-center text-purple-400 py-2">
+                        <span class="w-2/4 h-3 bg-purple-200 rounded-full"></span>
+                      </li>
+                      <li class="flex items-center text-purple-400 py-2">
+                        <span class="w-3/5 h-3 bg-purple-200 rounded-full"></span>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="col-span-full md:col-span-4 p-5 h-auto rounded border-2 border-purple-400 bg-purple-100">
+                    <div class="flex flex-col space-y-8">
+                      <div class="flex w-full space-x-5">
+                        <div class="flex flex-col items-center space-y-2">
+                          <i class="fas fa-arrow-alt-up text-purple-300 text-sm"></i>
+                          <div class="rounded-full bg-purple-200 w-2 h-2"></div>
+                          <i class="fas fa-arrow-alt-down text-purple-200 text-sm"></i>
+                        </div>
+                        <div class="flex flex-col w-full space-y-3">
+                          <div class="flex space-x-2 items-center">
+                            <div class="w-8 h-8 rounded bg-green-400"></div>
+                            <div class="w-24 h-3 bg-purple-300 rounded-full"></div>
+                          </div>
+                          <div class="w-3/4 h-3 bg-purple-300 rounded-full"></div>
+                          <div class="w-2/4 h-3 bg-purple-300 rounded-full"></div>
+                        </div>
+                        <div class="w-24 h-14 bg-purple-300 rounded"></div>
+                      </div>
+                      <div class="flex w-full space-x-5">
+                        <div class="flex flex-col items-center space-y-2">
+                          <i class="fas fa-arrow-alt-up text-purple-500 text-sm"></i>
+                          <div class="rounded-full bg-purple-500 w-2 h-2"></div>
+                          <i class="fas fa-arrow-alt-down text-purple-200 text-sm"></i>
+                        </div>
+                        <div class="flex flex-col w-full space-y-3">
+                          <div class="flex space-x-2 items-center">
+                            <div class="w-8 h-8 rounded bg-orange-300"></div>
+                            <div class="w-36 h-3 bg-purple-300 rounded-full"></div>
+                          </div>
+                          <div class="w-3/5 h-3 bg-purple-300 rounded-full"></div>
+                        </div>
+                      </div>
+                      <div class="flex w-full space-x-5">
+                        <div class="flex flex-col items-center space-y-2">
+                          <i class="fas fa-arrow-alt-up text-purple-200 text-sm"></i>
+                          <div class="rounded-full bg-purple-200 w-2 h-2"></div>
+                          <i class="fas fa-arrow-alt-down text-purple-200 text-sm"></i>
+                        </div>
+                        <div class="flex flex-col w-full space-y-3">
+                          <div class="flex space-x-2 items-center">
+                            <div class="w-8 h-8 rounded bg-pink-300"></div>
+                            <div class="w-32 h-3 bg-purple-300 rounded-full"></div>
+                          </div>
+                          <div class="w-5/6 h-3 bg-purple-300 rounded-full"></div>
+                          <div class="w-3/5 h-3 bg-purple-300 rounded-full"></div>
+                        </div>
+                        <div class="w-24 h-14 bg-purple-300 rounded"></div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="hidden md:block col-span-2 p-5 rounded border-2 border-purple-400 bg-purple-100">
+                    <ul class="mb-3">
+                      <li class="flex items-center text-purple-400 py-2">
+                        <span class="w-4/5 h-3 bg-purple-200 rounded-full"></span>
+                      </li>
+                      <li class="flex items-center text-purple-400 py-2">
+                        <span class="w-3/5 h-3 bg-purple-200 rounded-full"></span>
+                      </li>
+                      <li class="flex items-center text-purple-400 py-2">
+                        <span class="w-3/4 h-3 bg-purple-200 rounded-full"></span>
+                      </li>
+                    </ul>
+                    <ul>
+                      <li class="flex items-center space-x-2 py-2">
+                        <div class="w-8 h-8 rounded bg-green-300"></div>
+                        <span class="w-3/4 h-3 bg-purple-200 rounded-full"></span>
+                      </li>
+                      <li class="flex items-center space-x-2 py-2">
+                        <div class="w-8 h-8 rounded bg-orange-300"></div>
+                        <span class="w-2/5 h-3 bg-purple-200 rounded-full"></span>
+                      </li>
+                      <li class="flex items-center space-x-2 py-2">
+                        <div class="w-8 h-8 rounded bg-fuchsia-300"></div>
+                        <span class="w-3/4 h-3 bg-purple-200 rounded-full"></span>
+                      </li>
+                      <li class="flex items-center space-x-2 py-2">
+                        <div class="w-8 h-8 rounded bg-blue-300"></div>
+                        <span class="w-3/5 h-3 bg-purple-200 rounded-full"></span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
 			<button class="button primary" @click="router.push('/setup/2')">
 				Get started
 				<i class="far fa-long-arrow-right pl-2"></i>
