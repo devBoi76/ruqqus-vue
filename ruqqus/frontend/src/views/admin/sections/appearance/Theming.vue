0f2bd20bbbfc8e0057ab40bc33bfc18241182bc6
kek7198 <grayboy99@protonmail.com>
2021-08-17 14:39:31 -0500

    Update Theming.vue

--- ruqqus/frontend/src/views/admin/sections/appearance/Theming.vue
+++ ruqqus/frontend/src/views/admin/sections/appearance/Theming.vue
@@ -44,7 +44,7 @@
 					<div class="md:grid md:grid-cols-3 md:gap-6">
 						<div class="mt-5 md:mt-0 md:col-span-3 space-y-8">
 							<div>
-								<div class="text-xs font-medium uppercase tracking-wider text-gray-400 dark:text-gray-400 mb-2">
+								<div class="uppercase tracking-wide font-semibold text-sm md:text-xs text-gray-400 mb-2">
 									Theme
 								</div>
 								<div class="sm:rounded-sm sm:shadow-xs border-t border-b sm:border-0 bg-white">
