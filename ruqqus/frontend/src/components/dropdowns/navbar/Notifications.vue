6c03547e4a090d34e7b9cd8f8c1f9cdf1a4883f5
kek7198 <grayboy99@protonmail.com>
2021-08-17 16:49:11 -0500

    Update Notifications.vue

--- ruqqus/frontend/src/components/dropdowns/navbar/Notifications.vue
+++ ruqqus/frontend/src/components/dropdowns/navbar/Notifications.vue
@@ -57,7 +57,7 @@
               <!-- Loading State -->
               <div v-show="loading && !errored && !notifications.length" class="w-full space-y-4 py-4">
                 <div v-for="i in 5" :key="i" class="px-4 animate-pulse">
-                  <div class="rounded-sm bg-gray-200 dark:bg-white dark:bg-opacity-20 w-full h-8"></div>
+                  <div class="rounded-sm bg-gray-200 dark:bg-white dark:bg-opacity-20 w-full h-6"></div>
                 </div>
               </div>
               <!-- Errored State -->
