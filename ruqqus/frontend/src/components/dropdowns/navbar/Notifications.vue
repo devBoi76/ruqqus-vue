a23148564b80edf6d0ac5b397552d4d2ac8e88cf
kek7198 <grayboy99@protonmail.com>
2021-08-17 16:32:48 -0500

    styling

--- ruqqus/frontend/src/components/dropdowns/navbar/Notifications.vue
+++ ruqqus/frontend/src/components/dropdowns/navbar/Notifications.vue
@@ -1,127 +1,112 @@
 <template>
-  <div class="w-56 text-right fixed top-16">
-    <Menu as="div" class="relative inline-block text-left">
-      <div>
-        <MenuButton
-          class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-        >
-          <span class="sr-only">View notifications</span>
-          <!-- Button Icon -->
-          <i class="far fa-bell"></i>
-          <!-- Counter -->
-          <span class="absolute flex items-center justify-center w-6 h-6 rounded bg-purple-500 border-2 border-white dark:border-gray-800 transform transition duration-150" :class="notifications.length ? 'scale-100' : 'scale-0'" style="top: -6px;right: -6px;">
-            <span class="font-bold text-2xs text-white dark:text-gray-200">
-              {{ notifications.length || 1 }}
-            </span>
+  <Menu as="div" class="relative inline-block text-left">
+    <div>
+      <MenuButton
+        class="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+        <span class="sr-only">View notifications</span>
+        <!-- Button Icon -->
+        <i class="far fa-bell"></i>
+        <!-- Counter -->
+        <span class="absolute flex items-center justify-center w-6 h-6 rounded bg-purple-500 border-2 border-white dark:border-gray-800 transform transition duration-150" :class="notifications.length ? 'scale-100' : 'scale-0'" style="top: -6px;right: -6px;">
+          <span class="font-bold text-2xs text-white dark:text-gray-200">
+            {{ notifications.length || 1 }}
           </span>
-        </MenuButton>
-      </div>
+        </span>
+      </MenuButton>
+    </div>
 
-      <transition
-        enter-active-class="transition duration-100 ease-out"
-        enter-from-class="transform scale-95 opacity-0"
-        enter-to-class="transform scale-100 opacity-100"
-        leave-active-class="transition duration-75 ease-in"
-        leave-from-class="transform scale-100 opacity-100"
-        leave-to-class="transform scale-95 opacity-0"
-      >
-        <MenuItems
-          class="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-        >
-          <div class="px-1 py-1">
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex rounded-md items-center w-full px-2 py-2 text-sm',
-                ]"
-              >
-                <i class="far fa-sliders-h text-center fa-fw mr-2"></i><span>Your Preferences</span>
+    <transition
+      enter-active-class="transition duration-100 ease-out"
+      enter-from-class="transform scale-95 opacity-0"
+      enter-to-class="transform scale-100 opacity-100"
+      leave-active-class="transition duration-75 ease-in"
+      leave-from-class="transform scale-100 opacity-100"
+      leave-to-class="transform scale-95 opacity-0"
+    >
+      <MenuItems class="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div class="px-1 py-1">
+          <MenuItem v-slot="{ active }">
+            <button :class="[active ? 'bg-violet-500 text-white' : 'text-gray-900','group flex rounded-md items-center w-full px-2 py-2 text-sm',]">
+              <i class="far fa-sliders-h text-center fa-fw mr-2"></i><span>Your Preferences</span>
+            </button>
+          </MenuItem>
+          <MenuItem v-slot="{ active }">
+            <button :class="[active ? 'bg-violet-500 text-white' : 'text-gray-900','group flex rounded-md items-center w-full px-2 py-2 text-sm',]">
+              <i class="far fa-sign-out fa-rotate-180 text-center fa-fw mr-2"></i><span>Leave Guild</span>
+            </button>
+          </MenuItem>
+
+          <div class="flex items-center justify-between p-4 bg-white dark:bg-gray-900 border-b dark:border-gray-800">
+            <div class="text-2xs font-bold uppercase tracking-wider text-gray-700 dark:text-gray-400">Notifications</div>
+            <div class="flex items-center space-x-2 leading-tight">
+              <!-- Mark As Read Button -->
+              <button :disabled="!notifications.length" class="text-sm text-gray-500 hover:text-gray-700 disabed:opacity-50">
+                <i class="far fa-check-double"></i>
               </button>
-            </MenuItem>
-            <MenuItem v-slot="{ active }">
-              <button
-                :class="[
-                  active ? 'bg-violet-500 text-white' : 'text-gray-900',
-                  'group flex rounded-md items-center w-full px-2 py-2 text-sm',
-                ]"
-              >
-                <i class="far fa-sign-out fa-rotate-180 text-center fa-fw mr-2"></i><span>Leave Guild</span>
+              <span class="text-gray-300 text-sm font-hairline">
+                |
+              </span>
+              <!-- Toggle Preferences Button -->
+              <button class="text-sm text-gray-500 hover:text-gray-700" @click="toggleSettings()">
+                <i class="far fa-cog"></i>
               </button>
-            </MenuItem>
-
-            <div class="flex items-center justify-between p-4 bg-white dark:bg-gray-900 border-b dark:border-gray-800">
-              <div class="text-2xs font-bold uppercase tracking-wider text-gray-700 dark:text-gray-400">Notifications</div>
-              <div class="flex items-center space-x-2 leading-tight">
-                <!-- Mark As Read Button -->
-                <button :disabled="!notifications.length" class="text-sm text-gray-500 hover:text-gray-700 disabed:opacity-50">
-                  <i class="far fa-check-double"></i>
-                </button>
-                <span class="text-gray-300 text-sm font-hairline">
-                  |
-                </span>
-                <!-- Toggle Preferences Button -->
-                <button class="text-sm text-gray-500 hover:text-gray-700" @click="toggleSettings()">
-                  <i class="far fa-cog"></i>
-                </button>
-              </div>
             </div>
-
-            <!-- settings -->
-            <div v-show="showSettings" class="flex flex-col p-4 bg-white border-b">
-              <div class="text-2xs font-bold uppercase tracking-wide text-gray-400">
-                Show:
-              </div>
-              <hr>
-              <t-checkbox-group name="filter" v-model="filters" :options="['Replies', 'Mentions', 'Activity', 'Moderation']"></t-checkbox-group>
-            </div>
-
-            <!-- Notifications List -->
-            <vue-scroll :class="{'opacity-50 pointer-events-none':showSettings}">
-              <div class="flex flex-col items-center justify-center">
-                <!-- Loading State -->
-                <div v-show="loading && !errored && !notifications.length" class="w-full space-y-5 py-4">
-                  <div v-for="i in 5" :key="i" class="flex px-4 space-x-3 animate-pulse">
-                    <div class="rounded-sm bg-gray-200 dark:bg-white dark:bg-opacity-20 w-8 h-8 flex-shrink-0"></div>
-                    <div class="rounded-sm bg-gray-200 dark:bg-white dark:bg-opacity-20 w-full h-8"></div>
-                  </div>
-                </div>
-                <!-- Errored State -->
-                <div v-show="!loading && errored && !notifications.length">
-                  <div class="flex flex-col items-center text-center w-full p-12">
-                    <i class="block fad fa-ghost text-gray-400 text-4xl mb-5"></i>
-                    <div class="h5">Unable to Load Notifications</div>
-                    <p class="text-sm text-gray-400">
-                      Sorry, we're unable to fetch your notifications right now.
-                    </p>
-                    <button class="button outlineGray200 mt-3" @click="getNotifications()">
-                      <i class="far fa-redo-alt pr-2"></i>
-                      Try again
-                    </button>
-                  </div>
-                </div>
-                <!-- Notifications -->
-                <div v-if="notifications.length" class="w-full bg-white">
-                  <NotificationList :notifications="notifications" @click="hide"/>
-                </div>
-                <!-- Empty State -->
-                <div v-show="!loading && !errored && !notifications.length" class="flex flex-col items-center p-12">
-                  <i class="block fad fa-bell text-gray-400 text-4xl mb-5"></i>
-                  <div class="h5">
-                    No Unread Notifications
-                  </div>
-                  <p class="text-sm text-gray-400">
-                    You're all caught up! Time for a coffee break? 🧙
-                  </p>
-                </div>
-              </div>
-            </vue-scroll>
-
           </div>
-        </MenuItems>
-      </transition>
-    </Menu>
-  </div>
+
+          <!-- settings -->
+          <div v-show="showSettings" class="flex flex-col p-4 bg-white border-b">
+            <div class="text-2xs font-bold uppercase tracking-wide text-gray-400">
+              Show:
+            </div>
+            <hr>
+            <t-checkbox-group name="filter" v-model="filters" :options="['Replies', 'Mentions', 'Activity', 'Moderation']"></t-checkbox-group>
+          </div>
+
+          <!-- Notifications List -->
+          <div class="overflow-y-auto" :class="{'opacity-50 pointer-events-none':showSettings}">
+            <div class="flex flex-col items-center justify-center">
+              <!-- Loading State -->
+              <div v-show="loading && !errored && !notifications.length" class="w-full space-y-5 py-4">
+                <div v-for="i in 5" :key="i" class="flex px-4 space-x-3 animate-pulse">
+                  <div class="rounded-sm bg-gray-200 dark:bg-white dark:bg-opacity-20 w-8 h-8 flex-shrink-0"></div>
+                  <div class="rounded-sm bg-gray-200 dark:bg-white dark:bg-opacity-20 w-full h-8"></div>
+                </div>
+              </div>
+              <!-- Errored State -->
+              <div v-show="!loading && errored && !notifications.length">
+                <div class="flex flex-col items-center text-center w-full p-12">
+                  <i class="block fad fa-ghost text-gray-400 text-4xl mb-5"></i>
+                  <div class="h5">Unable to Load Notifications</div>
+                  <p class="text-sm text-gray-400">
+                    Sorry, we're unable to fetch your notifications right now.
+                  </p>
+                  <button class="button outlineGray200 mt-3" @click="getNotifications()">
+                    <i class="far fa-redo-alt pr-2"></i>
+                    Try again
+                  </button>
+                </div>
+              </div>
+              <!-- Notifications -->
+              <div v-if="notifications.length" class="w-full bg-white">
+                <NotificationList :notifications="notifications" @click="hide"/>
+              </div>
+              <!-- Empty State -->
+              <div v-show="!loading && !errored && !notifications.length" class="flex flex-col items-center p-12">
+                <i class="block fad fa-bell text-gray-400 text-4xl mb-5"></i>
+                <div class="h5">
+                  No Unread Notifications
+                </div>
+                <p class="text-sm text-gray-400">
+                  You're all caught up! Time for a coffee break? 🧙
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </MenuItems>
+    </transition>
+  </Menu>
 </template>
 
 <script>
--- ruqqus/frontend/src/components/dropdowns/navbar/Profile.vue
+++ ruqqus/frontend/src/components/dropdowns/navbar/Profile.vue
@@ -39,7 +39,7 @@
     <MenuItem v-slot="{ active }">
       <button
       :class="[
-      active ? 'bg-violet-500 text-white' : 'text-gray-700 hover:text-gray-900',
+      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
       'group flex rounded-md items-center w-full px-2 py-2 text-sm',
       ]"
       >
@@ -50,7 +50,7 @@
   <MenuItem v-slot="{ active }">
     <button
     :class="[
-    active ? 'bg-violet-500 text-white' : 'text-gray-700 hover:text-gray-900',
+    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
     'group flex rounded-md items-center w-full px-2 py-2 text-sm',
     ]"
     >
@@ -61,7 +61,7 @@
 <MenuItem v-slot="{ active }">
   <button
   :class="[
-  active ? 'bg-violet-500 text-white' : 'text-gray-700 hover:text-gray-900',
+  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
   'group flex rounded-md items-center w-full px-2 py-2 text-sm',
   ]"
   >
@@ -72,7 +72,7 @@
 <MenuItem v-slot="{ active }">
   <button
   :class="[
-  active ? 'bg-violet-500 text-white' : 'text-gray-700 hover:text-gray-900',
+  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
   'group flex rounded-md items-center w-full px-2 py-2 text-sm',
   ]"
   >
@@ -83,7 +83,7 @@
 <MenuItem v-slot="{ active }">
   <button
   :class="[
-  active ? 'bg-violet-500 text-white' : 'text-gray-700 hover:text-gray-900',
+  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
   'group flex rounded-md items-center w-full px-2 py-2 text-sm',
   ]"
   >
@@ -94,7 +94,7 @@
 <MenuItem v-slot="{ active }">
   <button
   :class="[
-  active ? 'bg-violet-500 text-white' : 'text-gray-700 hover:text-gray-900',
+  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
   'group flex rounded-md items-center w-full px-2 py-2 text-sm',
   ]"
   >
@@ -105,7 +105,7 @@
 <MenuItem v-slot="{ active }">
   <button
   :class="[
-  active ? 'bg-violet-500 text-white' : 'text-gray-700 hover:text-gray-900',
+  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
   'group flex rounded-md items-center w-full px-2 py-2 text-sm',
   ]"
   >
@@ -116,7 +116,7 @@
 <MenuItem v-slot="{ active }">
   <button
   :class="[
-  active ? 'bg-violet-500 text-white' : 'text-gray-700 hover:text-gray-900',
+  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
   'group flex rounded-md items-center w-full px-2 py-2 text-sm',
   ]"
   >
@@ -129,7 +129,7 @@
   <MenuItem v-slot="{ active }">
     <button
     :class="[
-    active ? 'bg-violet-500 text-white' : 'text-gray-700 hover:text-gray-900',
+    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
     'group flex rounded-md items-center w-full px-2 py-2 text-sm',
     ]"
     >
