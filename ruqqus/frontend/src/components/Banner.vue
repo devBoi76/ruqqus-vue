4f9d1097f058124a3da59e8df32c17288cf29608
kek7198 <grayboy99@protonmail.com>
2021-08-18 12:36:34 -0500

    styling

--- ruqqus/frontend/src/components/Banner.vue
+++ ruqqus/frontend/src/components/Banner.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="relative flex flex-col items-center justify-center text-center min-h-[200px] max-h-[240px] sm:min-h-[270px] sm:max-h-[340px] z-50">
 		<img class="absolute w-full h-full bottom-0 top-0 left-0 right-0 object-cover" :src="banner.image" alt="banner">
-		<h1 class="z-10 text-white mb-4">
+		<h1 class="hidden sm:block z-10 text-white mb-4">
 			{{ banner.title }}
 		</h1>
-		<p class="z-10 text-white text-3xl text-opacity-80 max-w-[600px] mx-auto">
+		<p class="z-10 text-white text-xl sm:text-3xl text-opacity-80 max-w-[600px] mx-auto">
 			{{ banner.subtitle }}
 		</p>
 	</div>
