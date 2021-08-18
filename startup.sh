ce6d341f3c73f5c4c435acc9083fd0a62d376db3
arkit3kt <exodust@protonmail.com>
2021-08-18 00:08:27 -0400

    startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd ~/app/ruqqus-vue/
+echo "Pulling in Frontend changes"
+git pull
+echo "Install Frontend Reqs"
 sudo npm install --unsafe-perm=true --allow-root
 sudo npm audit fix
 sudo npm fund
+echo "Building Vue"
+rm -rf src/assets/dist
 npm run build
-cp -r dist/ ~/app/ruqqus-vue/ruqqus/assets
+cp -r dist/ src/assets
