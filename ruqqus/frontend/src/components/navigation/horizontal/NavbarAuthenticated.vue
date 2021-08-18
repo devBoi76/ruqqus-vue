e172274033c08d7c8c5baecaf099cccf09588bf8
kek7198 <grayboy99@protonmail.com>
2021-08-18 17:50:30 -0500

    styling

--- ruqqus/frontend/src/components/navigation/horizontal/NavbarAuthenticated.vue
+++ ruqqus/frontend/src/components/navigation/horizontal/NavbarAuthenticated.vue
@@ -18,7 +18,7 @@
 
 						<router-link to="/" tag="button" class="flex items-center justify-center px-2 w-8 h-8 text-xl text-gray-600 dark:text-gray-400 hover:text-purple-500 dark:hover:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white rounded-sm" content="Explore" v-tippy="{ placement: 'bottom', delay: [1000,0] }">
 							<span class="sr-only">Home</span>
-							<i class="far fa-home-lg-alt"></i>
+							<i class="far fa-home-alt"></i>
 						</router-link>
 
 						<router-link to="/?sort=hot" tag="button" class="flex items-center justify-center px-2 w-8 h-8 text-xl text-gray-600 dark:text-gray-400 hover:text-purple-500 dark:hover:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white rounded-sm" content="Explore" v-tippy="{ placement: 'bottom', delay: [1000,0] }">
