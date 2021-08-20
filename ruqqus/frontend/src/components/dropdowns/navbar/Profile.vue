db8edf6d8719ff7ad675175fe6a5066742e88c2e
kek7198 <grayboy99@protonmail.com>
2021-08-19 19:09:40 -0500

    test

--- ruqqus/frontend/src/components/dropdowns/navbar/Profile.vue
+++ ruqqus/frontend/src/components/dropdowns/navbar/Profile.vue
@@ -78,7 +78,7 @@
         <!-- Rules -->
         <MenuItem>
           <router-link to="/rules" custom v-slot="{ navigate }">
-            <button class="text-gray-700 hover:text-gray-900 hover:bg-gray-100 group flex items-center w-full px-4 py-1.5" @click="navigate" @keypress.enter="navigate" role="link">
+            <button class="text-gray-700 hover:text-gray-900 hover:bg-gray-100 focus:text-gray-900 focus:bg-gray-100 active:text-gray-900 active:bg-gray-100 group flex items-center w-full px-4 py-1.5" @click="navigate" @keypress.enter="navigate" role="link">
               <i class="far fa-scroll-old text-center fa-fw mr-2"></i><span>Rules</span>
             </button>
           </router-link>
