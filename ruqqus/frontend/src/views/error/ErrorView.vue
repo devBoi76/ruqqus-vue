24a8c7f731abeb7a7e0126a83dce61983eeff5cf
kek7198 <grayboy99@protonmail.com>
2021-08-20 12:00:32 -0500

    text

--- ruqqus/frontend/src/views/error/ErrorView.vue
+++ ruqqus/frontend/src/views/error/ErrorView.vue
@@ -4,13 +4,13 @@
 		<div class="relative flex flex-col items-center justify-center text-center h-full z-50">
 			<img class="absolute w-full h-full bottom-0 top-0 left-0 right-0 object-cover" src="https://i.imgur.com/AgS5BXJ.jpg" alt="banner"/>
 			<div v-if="statusCode" class="z-10 uppercase tracking-wide font-semibold text-gray-400 mb-2">
-				404
+				{{ statusCode }}
 			</div>
 			<h1 class="hidden sm:block z-10 text-white mb-4">
 				{{ heading || 'An error occured' }}
 			</h1>
 			<p class="z-10 text-white text-2xl sm:text-3xl text-opacity-80 max-w-[600px] mx-auto">
-				{{ statusCode || 'Some sort of error occured...' }}
+				{{ message || 'Some sort of error occured...' }}
 			</p>
 			<div class="z-10 mx-auto mt-4">
 				<router-link to="/" custom v-slot="{ navigate }">
