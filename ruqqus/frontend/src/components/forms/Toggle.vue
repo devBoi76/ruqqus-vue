84040b604e02ffe13002e8b40c1e9dbdf3a4ebea
kek7198 <grayboy99@protonmail.com>
2021-08-20 17:08:15 -0500

    styling

--- ruqqus/frontend/src/components/forms/Toggle.vue
+++ ruqqus/frontend/src/components/forms/Toggle.vue
@@ -1,5 +1,5 @@
 <template>
-  <Switch v-model="enabled" :class="enabled ? 'bg-green-900' : 'bg-green-700'" class="relative inline-flex flex-shrink-0 h-[22px] w-[42px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+  <Switch v-model="enabled" :class="enabled ? 'bg-green-500' : 'bg-gray-300'" class="relative inline-flex flex-shrink-0 h-[22px] w-[42px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
     <span class="sr-only">Use setting</span>
     <span aria-hidden="true" :class="enabled ? 'translate-x-9' : 'translate-x-0'" class="pointer-events-none inline-block h-[18px] w-[18px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200"/>
   </Switch>
