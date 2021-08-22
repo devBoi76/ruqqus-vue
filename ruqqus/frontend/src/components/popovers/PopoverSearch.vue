c0c5990823311d3dbd143bbc6773649c110100e8
kek7198 <grayboy99@protonmail.com>
2021-08-21 19:58:53 -0500

    styling

--- ruqqus/frontend/src/components/popovers/PopoverSearch.vue
+++ ruqqus/frontend/src/components/popovers/PopoverSearch.vue
@@ -1,6 +1,6 @@
 <template>
 	<Popover v-slot="{ open }">
-		<PopoverButton as="div">
+		<PopoverButton as="div" class="w-96 appearance-none">
 			<input type="text" class="form-input light" v-model="searchTerm" placeholder="Search posts or @users" @keyup.enter="search()"/>
 		</PopoverButton>
 
@@ -15,20 +15,24 @@
 		>
 		<div v-if="open">
 			<!-- Don't forget to add static to your PopoverPanel! -->
-			<PopoverPanel static class="absolute right-0 w-96 z-10 bg-white border rounded shadow-lg">
-				<div class="flex items-center px-4 py-3">
-					<div class="uppercase tracking-wide font-semibold leading-none text-2xs text-gray-500">
-						Recent searches
+			<PopoverPanel static class="absolute right-0 w-96 z-10 mt-2 bg-white border rounded shadow-lg">
+				<div class="divide-y">
+					<div class="flex items-center px-4 py-3">
+						<div class="uppercase tracking-wide font-semibold leading-none text-2xs text-gray-500">
+							Recent searches
+						</div>
+						<div class="ml-auto">
+							<button class="text-sm text-gray-500">
+								Clear
+							</button>
+						</div>
 					</div>
-					<div class="ml-auto">
-						Clear
+					<div class="py-2">
+						<button class="'group flex items-center w-full px-4 py-1.5 text-gray-700 hover:text-gray-900 hover:bg-gray-100">
+							<i class="far fa-search fa-fw text-gray-500 mr-2"></i>
+							<span>search term</span>
+						</button>
 					</div>
-				</div>
-				<div class="px-4 py-2">
-					<button class="'group flex items-center w-full px-4 py-1.5 text-gray-700 hover:text-gray-900 hover:bg-gray-100">
-						<i class="far fa-search text-center fa-fw mr-2"></i>
-						<span>search term</span>
-					</button>
 				</div>
 			</PopoverPanel>
 		</div>
