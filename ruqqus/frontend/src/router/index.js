d1564f054bf597ee10f3359537c1e402ac52eefc
kek7198 <grayboy99@protonmail.com>
2021-08-22 16:29:01 -0500

    integrations

--- ruqqus/frontend/src/router/index.js
+++ ruqqus/frontend/src/router/index.js
@@ -56,6 +56,7 @@ const AdminView = () => import ('../views/admin/AdminView.vue')
 const AdminModerationView = () => import ('../views/admin/Moderation.vue')
 const AdminQueueView = () => import ('../views/admin/sections/queue/Queue.vue')
 const AdminGeneralView = () => import ('../views/admin/sections/appearance/General.vue')
+const AdminIntegrationsView = () => import ('../views/admin/Integrations.vue')
 const AdminThemingView = () => import ('../views/admin/sections/appearance/Theming.vue')
 const AdminUserManagementView = () => import ('../views/admin/sections/members/Members.vue')
 const AdminSecurityView = () => import ('../views/admin/sections/other/Security.vue')
@@ -143,6 +144,7 @@ const routes = [
 			{ path: '', component: AdminGeneralView, name: 'admin-general-view', props: true, meta: {requiresAuth: true, sidebar: false, dropImage: false} },
 			{ path: '/admin/general', component: AdminGeneralView, name: 'admin-general-view', props: true, meta: {requiresAuth: true, sidebar: false, dropImage: false} },
 			{ path: '/admin/moderation', component: AdminModerationView, name: 'admin-moderation-view', props: true, meta: {requiresAuth: true, sidebar: false, dropImage: false} },
+			{ path: '/admin/integrations/:integration?', component: AdminIntegrationsView, name: 'admin-integrations-view', props: true, meta: {requiresAuth: true, sidebar: false, dropImage: false} },
 			{ path: '/admin/queue/:filter?', component: AdminQueueView, name: 'admin-queue-view', props: true, meta: {requiresAuth: true, sidebar: false, dropImage: false} },
 			{ path: '/admin/theming', component: AdminThemingView, name: 'admin-theming-view', props: true, meta: {requiresAuth: true, sidebar: false, dropImage: false} },
 			{ path: '/admin/members/:sort?', component: AdminUserManagementView, name: 'admin-user-management-view', props: true, meta: {requiresAuth: true, sidebar: false, dropImage: false} },
---
+++ ruqqus/frontend/src/views/admin/Integrations.vue
@@ -0,0 +1,178 @@
+<template>
+	<div class="flex flex-col flex-grow overflow-y-auto">
+
+		<!-- Header content section -->
+		<div class="grid grid-cols-12 sticky top-0 z-10">
+			<div class="col-span-full 2xl:col-start-2 2xl:col-end-10">
+				<div class="flex items-center justify-between px-4 py-3 bg-gray-50 sm:bg-gray-100">
+					<h1 class="text-2xl capitalize mb-0">
+						Integrations
+					</h1>
+					<div class="flex items-center space-x-4">
+						<div v-show="changed" class="text-xs text-gray-400">
+							You have unsaved changes!
+						</div>
+						<button v-if="!loading && !errored" :disabled="!changed" class="button purple500" @click="save()">
+							Save
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Main content section -->
+
+		<!-- Error state if guild data fails to load -->
+		<div v-show="errored && !loading" class="w-full h-2/3 px-4">
+			<div class="flex flex-col h-full justify-center items-center text-center">
+				<i class="block fad fa-ghost text-gray-400 dark:text-gray-200 text-5xl mb-6"></i>
+				<div class="h5">
+					Unable to Load Moderation Tools
+				</div>
+				<p class="text-gray-600 dark:text-gray-400">
+					Sorry, we're unable to fetch guild reports right now. Please try again later.
+				</p>
+			</div>
+		</div>
+
+		<div class="grid grid-cols-12">
+
+			<div v-show="loading && !errored" class="col-span-full 2xl:col-start-2 2xl:col-end-10">
+				<div class="flex flex-col w-full p-4 space-y-3 animate-pulse">
+					<div class="rounded-sm bg-gray-200 dark:bg-white dark:bg-opacity-20 w-1/6 h-3"></div>
+					<div class="rounded-sm bg-gray-200 dark:bg-white dark:bg-opacity-20 w-full h-20"></div>
+				</div>
+			</div>
+
+			<div v-if="!loading && !errored" class="col-span-full 2xl:col-start-2 2xl:col-end-10">
+				<div class="p-4">
+					<div class="md:grid md:grid-cols-3 md:gap-6">
+
+						<div>
+							<div class="uppercase tracking-wide font-semibold text-sm md:text-xs text-gray-400 mb-2">
+								Available Integrations
+							</div>
+							<div class="flex flex-wrap -mx-2 overflow-hidden">
+								<div class="my-2 px-2 w-1/2 overflow-hidden">
+									<div v-for="(item, index) in integrations" :key="index" class="my-2 px-2 w-1/2 overflow-hidden lg:w-1/3">
+										<div class="flex p-5 bg-white border rounded-sm">
+											<div class="flex">
+												<img :src="item.logo" class="w-24 h-24 obeject-cover mb-2"/>
+												<div>
+													<div class="h4">
+														{{ item.name }}
+													</div>
+													<p class="text-gray-500 mt-2">
+														{{ item.description }}
+													</p>
+												</div>
+											</div>
+											<button class="button gray400 w-full mt-4">
+												<i class="far fa-plus fa-fw pr-2"></i>
+												Connect
+											</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div>
+							<div class="uppercase tracking-wide font-semibold text-sm md:text-xs text-gray-400 mb-2">
+								Your Integrations
+							</div>
+							<div class="sm:rounded-sm sm:shadow-xs border-t border-b sm:border-0 bg-white">
+								<div class="p-4 border-b">
+									<div class="flex flex-grow items-center justify-between">
+										<div>
+											<div class="font-semibold leading-tight">
+												Restrict posting
+											</div>
+											<p class="text-sm text-gray-500 mt-1">
+												Restrict posting to approved users only
+											</p>
+										</div>
+										<div class="flex items-center space-x-2">
+											<Toggle v-model="s.canPost"/>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</template>
+
+<script>
+// Import components
+//import { getGuild } from '@/api/Guild.js';
+
+import Toggle from "@/components/forms/Toggle.vue";
+
+export default {
+	name: "AdminIntegrationsView",
+	data() {
+		return {
+			changed: false,
+			loading: false,
+			errored: false,
+			s: {},
+			saved: {},
+			integrations: [
+			{
+				logo: 'https://i.imgur.com/bV2OM6P.png',
+				name: 'Gumroad',
+				description: 'Collect donations from your members via automatic monthly subscriptions.'
+				isActive: false
+			}
+			]
+		}
+	},
+	components: {
+		Toggle
+	},
+	watch: {
+		// '$route.params.name': { // get guild info and posts if guild changes
+		// 	handler() {
+		// 		this.getGuildInfo()
+		// 	},
+		// 	immediate: true
+		// },
+		's': { // get guild info and posts if guild changes
+			handler() {
+				this.changed = (JSON.stringify(this.s) !== JSON.stringify(this.saved))
+			},
+			deep: true
+		}
+	},
+	methods: {
+		// getGuildInfo() {
+		// 	let guild = this.$route.params.name;
+		// 	getGuild(guild)
+		// 	.then(response => {
+		// 		let data = response.data
+		// 		this.saved = Object.assign({}, data);
+		// 		this.g = data;
+		// 	})
+		// 	.catch(error => {
+		// 		console.error(error)
+		// 		this.errored = true
+		// 	})
+		// 	.finally(() => this.loading = false)
+		// },
+		save() {
+			this.changed = false;
+			this.saved = Object.assign({}, this.s);
+			this.$store.commit('site/SET_SITE', {site: this.saved});
+		}
+	},
+	created() {
+		this.s = Object.assign({}, this.$store.getters['site/getSite'])
+	}
+};
+</script>
