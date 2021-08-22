edf8d1a4d21d7da701426b6819d6f2528cf5c6dd
kek7198 <grayboy99@protonmail.com>
2021-08-22 17:50:03 -0500

    Update DropAnImage.vue

--- ruqqus/frontend/src/components/DropAnImage.vue
+++ ruqqus/frontend/src/components/DropAnImage.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="{'absolute bottom-0 w-full h-full':showDropzone}">
-    <div class="absolute flex items-center justify-center overflow-hidden w-full h-screen z-100 bg-opacity-80" :class="wrapperClass" @drop.prevent="drop($event)" @click.self="wrongFileReset()" @keydown.esc="showDropzone=false; reset()" tabindex="0">
+    <div class="absolute flex items-center justify-center overflow-hidden w-full h-screen z-100 bg-opacity-80 transition-all duration-200" :class="wrapperClass" @drop.prevent="drop($event)" @click.self="wrongFileReset()" @keydown.esc="showDropzone=false; reset()" tabindex="0">
       <transition
       enter-active-class="transition duration-100 ease-out"
       enter-from-class="transform scale-95 opacity-0"
