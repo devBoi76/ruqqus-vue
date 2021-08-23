1ee74639d6611792b83d4075caaf70d772775244
kek7198 <grayboy99@protonmail.com>
2021-08-23 14:13:26 -0500

    styling

--- ruqqus/frontend/src/views/admin/integrations/configs/Ko-Fi.vue
+++ ruqqus/frontend/src/views/admin/integrations/configs/Ko-Fi.vue
@@ -92,7 +92,7 @@
 								Configuration
 							</div>
 							<div class="relative rounded-sm border bg-white">
-								<div v-show="!isActive" class="absolute w-full h-full bg-white bg-opacity-80 backdrop-blur-sm z-10">
+								<div v-show="!isActive" class="absolute w-full h-full rounded-sm bg-white bg-opacity-80 backdrop-blur-sm z-10">
 									<div class="w-full h-full flex flex-col items-center justify-center">
 										<p class="text-gray-500">
 											You have not configured Ko-Fi with your site.
