e5deb6d2be42ab8204d0e410cc4d85528d742e52
kek7198 <grayboy99@protonmail.com>
2021-08-23 11:11:51 -0500

    template

--- ruqqus/frontend/src/views/admin/integrations/Integrations.vue
+++ ruqqus/frontend/src/views/admin/integrations/Integrations.vue
@@ -1,63 +1,12 @@
 <template>
 	<div class="flex flex-col flex-grow overflow-y-auto">
-		<!-- Header content section -->
-		<div class="grid grid-cols-12 sticky top-0 z-10">
-			<div class="col-span-full 2xl:col-start-2 2xl:col-end-10">
-				<div class="flex items-center justify-between px-4 py-3 bg-gray-50 sm:bg-gray-100">
-					<div class="flex items-center">
-						<h1 class="text-2xl capitalize mb-0">
-							Integrations
-						</h1>
-						<div v-if="$route.meta.title" class="pl-4 flex items-center space-x-4">
-							<i class="fal fa-chevron-right fa-lg text-gray-500"></i>
-							<h2 class="text-2xl capitalize mb-0">
-								{{ $route.meta.title }}
-							</h2>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- Main content section -->
-		<!-- Error state if community data fails to load -->
-		<div v-show="errored && !loading" class="w-full h-2/3 px-4">
-			<div class="flex flex-col h-full justify-center items-center text-center">
-				<i class="block fad fa-ghost text-gray-400 dark:text-gray-200 text-5xl mb-6"></i>
-				<div class="h5">
-					Unable to Load Moderation Tools
-				</div>
-				<p class="text-gray-600 dark:text-gray-400">
-					Sorry, we're unable to fetch guild reports right now. Please try again later.
-				</p>
-			</div>
-		</div>
-		<!-- Content -->
-		<div class="grid grid-cols-12">
-			<div v-show="loading && !errored" class="col-span-full 2xl:col-start-2 2xl:col-end-10">
-				<div class="flex flex-col w-full p-4 space-y-3 animate-pulse">
-					<div class="rounded-sm bg-gray-200 dark:bg-white dark:bg-opacity-20 w-1/6 h-3"></div>
-					<div class="rounded-sm bg-gray-200 dark:bg-white dark:bg-opacity-20 w-full h-20"></div>
-				</div>
-			</div>
-			<div v-if="!loading && !errored" class="col-span-full 2xl:col-start-2 2xl:col-end-10">
-				<div class="p-4">
-					<div class="md:grid md:grid-cols-3 md:gap-6">
-						<div class="mt-5 md:mt-0 md:col-span-3 space-y-8">
-							<router-view></router-view>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+		<router-view></router-view>
 	</div>
 </template>
 
 <script>
 	export default {
 		name: "AdminIntegrationsView",
-		props: {
-			name: String
-		},
 		data() {
 			return {
 				changed: false,
