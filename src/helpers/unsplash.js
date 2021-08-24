7900f83994afc23e1d807e8436b440b46a07425f
kek7198 <grayboy99@protonmail.com>
2021-08-24 13:38:52 -0500

    test

--- src/helpers/unsplash.js
+++ src/helpers/unsplash.js
@@ -3,7 +3,9 @@
 
 import axios from 'axios'
 // Return a random Unsplash photo given search query
-export function getRandomPhoto(query, id){
+const id = '9-1rAdvT2NwaxLIumU1kFAqHILRlBFJDRMoMaCdErug'
+
+export function getRandomPhoto(query){
     const url = `https://api.unsplash.com/photos/random?query=${query}&client_id=${id}`
     return axios.get(url);
 }
