e105d80db9cb5ceec84a6b139dd7c7433e04ee73
kek7198 <grayboy99@protonmail.com>
2021-08-26 09:30:29 -0500

    Update Banner.vue

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="relative flex flex-col items-center justify-center text-center min-h-[100px] max-h-[140px] sm:min-h-[270px] sm:max-h-[300px] z-50">
-		<img class="absolute w-full h-full bottom-0 top-0 left-0 right-0 object-cover" :src="image" alt="banner">
+		<img class="absolute w-full h-full bottom-0 top-0 left-0 right-0 object-cover" :src="image || site.cover" alt="banner">
 		<h1 class="hidden sm:block z-10 text-white mb-4">
-			{{ title }}
+			{{ title || site.name }}
 		</h1>
 		<p class="z-10 text-white text-2xl sm:text-3xl text-opacity-80 max-w-[600px] mx-auto">
-			{{ subtitle }}
+			{{ subtitle || site.subtitle }}
 		</p>
 	</div>
 </template>
@@ -13,23 +13,9 @@
 <script>
 	export default {
 		props: {
-			image: {
-				type: String,
-				default: this.site.cover
-			},
-			title: {
-				type: String,
-				default: this.site.name
-			},
-			subtitle: {
-				type: String,
-				subtitle: this.site.tagline
-			}
-		},
-		computed: {
-			site() {
-				return this.$store.getters['persist/getSite'];
-			}
+			image: String,
+			title: String,
+			subtitle: String
 		}
 	}
 </script>
