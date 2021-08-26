8f007ed1bb90ce47622078306e8c0bd470abe0a5
kek7198 <grayboy99@protonmail.com>
2021-08-25 21:01:42 -0500

    styling

--- src/views/admin/sections/settings/integrations/IntegrationsList.vue
+++ src/views/admin/sections/settings/integrations/IntegrationsList.vue
@@ -36,31 +36,33 @@
 			<div class="p-4">
 				<div class="md:grid md:grid-cols-3 md:gap-6">
 					<div class="md:col-span-3 space-y-8">
-						<div class="relative flex overflow-x-scroll scrollbar-hidden">
-							<div class="absolute top-0 -right-4 w-20 bg-gradient-to-r from-white"></div>
-							<div class="absolute top-0 left-0 w-10 bg-gradient-to-l from-white"></div>
-							<ul class="flex flex-nowrap mb-0">
-								<li v-for="(item, index) in integrations" :key="index" v-show="!item.isActive" class="my-2 px-2 w-full w-72">
-									<div class="flex flex-col justify-between h-full sm:p-4 p-5 bg-white border rounded-sm transition-all duration-100 hover:shadow-lg">
-										<div class="flex flex-col items-center mb-4">
-											<img :src="item.logo" class="w-9 h-9 object-cover"/>
-											<div class="text-center mt-2">
-												<div class="font-semibold">
-													{{ item.name }}
+						<div class="relative">
+							<div class="absolute top-0 -right-4 w-20 h-full bg-gradient-to-l from-gray-100"></div>
+							<div class="absolute top-0 left-0 w-10 h-full bg-gradient-to-r from-gray-100"></div>
+							<div class="flex overflow-x-scroll scrollbar-hidden">
+								<ul class="flex flex-nowrap mb-0">
+									<li v-for="(item, index) in integrations" :key="index" v-show="!item.isActive" class="my-2 px-2 w-full w-72">
+										<div class="flex flex-col justify-between h-full sm:p-4 p-5 bg-white border rounded-sm transition-all duration-100 hover:shadow-lg">
+											<div class="flex flex-col items-center mb-4">
+												<img :src="item.logo" class="w-9 h-9 object-cover"/>
+												<div class="text-center mt-2">
+													<div class="font-semibold">
+														{{ item.name }}
+													</div>
+													<p class="text-sm text-gray-500 mt-1">
+														{{ item.description }}
+													</p>
 												</div>
-												<p class="text-sm text-gray-500 mt-1">
-													{{ item.description }}
-												</p>
 											</div>
+											<router-link :to="`/admin/integrations/${item.name}`" custom v-slot="{ navigate }">
+												<button class="button green500" @click="navigate" @keypress.enter="navigate" role="link">
+													Connect
+												</button>
+											</router-link>
 										</div>
-										<router-link :to="`/admin/integrations/${item.name}`" custom v-slot="{ navigate }">
-											<button class="button green500" @click="navigate" @keypress.enter="navigate" role="link">
-												Connect
-											</button>
-										</router-link>
-									</div>
-								</li>
-							</ul>
+									</li>
+								</ul>
+							</div>
 						</div>
 						<div>
 							<div class="uppercase tracking-wide font-semibold text-sm md:text-xs text-gray-400 mb-2">
