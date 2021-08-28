7671093389002b41853c0867cada13afa07e18c9
kek7198 <grayboy99@protonmail.com>
2021-08-28 14:36:25 -0500

    Update FeedView.vue

--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -8,9 +8,9 @@
 				<div class="w-full">
 
 					<!-- Post Creation -->
-					<router-link to="/submit" v-if="is_authenticated" class="hidden sm:flex items-center mb-4 px-4 py-3 bg-white dark:bg-gray-800 sm:border sm:rounded-sm">
+					<router-link to="/submit" v-if="is_authenticated" class="group hidden sm:flex items-center mb-4 px-4 py-3 bg-white dark:bg-gray-800 sm:border sm:rounded-sm">
 						<img :src="v.profile_url" class="flex-shrink-0 w-6 h-6 object-fit mr-3 rounded-sm bg-gray-200 dark:bg-gray-800">
-						<div class="group flex-grow overflow-hidden">
+						<div class="flex-grow overflow-hidden">
 							<div class="flex items-center h-full">
 								<p class="text-gray-400 group-hover:text-gray-500 dark:group-hover:text-gray-200 mb-0">
 									Create a new post...
