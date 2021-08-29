186f4b65798d14feb73504b1585b8104f544bed1
kek7198 <grayboy99@protonmail.com>
2021-08-29 11:23:21 -0500

    fix

--- src/components/forms/Toggle.vue
+++ src/components/forms/Toggle.vue
@@ -1,6 +1,6 @@
 <template>
   <Switch v-model="enabled" :class="[enabled ? 'bg-green-500' : 'bg-gray-200', { 'opacity-50 pointer-events-none':disabled }]" class="relative inline-flex flex-shrink-0 h-[20px] w-[38px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-    <i class="absolute top-[3px] left-[4px] fas fa-check text-[10px] text-white transform" :class="enabled ? 'scale-0 opacity-0' : 'scale-100 opacity-100'"></i>
+    <i class="absolute top-[3px] left-[4px] fas fa-check text-[10px] text-white transform" :class="enabled ? 'scale-100 opacity-100' : 'scale-0 opacity-0'"></i>
     <span class="sr-only">Use setting</span>
     <span aria-hidden="true" :class="enabled ? 'translate-x-[18px]' : 'translate-x-0'" class="pointer-events-none inline-block h-[16px] w-[16px] rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"/>
   </Switch>
