007aa20a1e3f889a5779f78b6e57df68a9d350d8
kek7198 <grayboy99@protonmail.com>
2021-08-29 11:10:47 -0500

    remove code

--- src/views/user/settings/Settings.vue
+++ src/views/user/settings/Settings.vue
@@ -7,42 +7,6 @@
         </h1>
         <!-- Main Content Section -->
         <div class="w-full space-y-8">
-          <div class="relative">
-            <div class="z-10 absolute top-4 right-4">
-              <button v-show="!editAppearance" class="button white" @click="toggleAppearance">
-                Edit appearance
-              </button>
-              <div v-show="editAppearance" class="flex items-center space-x-2">
-                <button type="button" class="button linkWhite" @click="toggleAppearance" tabindex="0">
-                  Cancel
-                </button>
-                <button type="button" class="button white" tabindex="0">
-                  Save
-                </button>
-              </div>
-            </div>
-            <div class="relative">
-              <img :src="v.banner_url" class="w-full h-56 lg:h-72 object-cover rounded"/>
-              <div v-show="editAppearance" class="absolute bottom-0 flex items-center justify-center w-full h-full bg-black bg-opacity-30">
-                <button type="button" class="w-9 h-9 flex items-center justify-center px-2 py-0 text-white bg-transparent hover:bg-black hover:bg-opacity-50 rounded-sm transition duration-100 ease-in-out" tabindex="0">
-                  <i class="far fa-pen fa-lg"></i>
-                </button>
-              </div>
-            </div>
-            <div class="flex px-4 -mt-12">
-              <div class="relative rounded-sm overflow-hidden w-20 h-20 flex-shrink-0">
-                <img :src="v.profile_url" class="object-cover" alt="profile-picture"/>
-                <div v-show="editAppearance" class="absolute bottom-0 flex items-center justify-center w-full h-full bg-black bg-opacity-30">
-                  <button type="button" class="w-9 h-9 flex items-center justify-center text-white px-2 py-0 bg-transparent hover:bg-black hover:bg-opacity-50 rounded-sm transition duration-100 ease-in-out" tabindex="0">
-                    <i class="far fa-pen fa-lg"></i>
-                  </button>
-                </div>
-              </div>
-              <div class="z-10 ml-4 pt-3 font-bold text-lg text-white" :class="{'text-opacity-30':editAppearance}">
-                {{ v.username }}
-              </div>
-            </div>
-          </div>
           <div>
             <div class="flex justify-between">
               <div class="uppercase tracking-wide font-semibold text-sm md:text-xs text-gray-400 mb-2">
@@ -329,7 +293,6 @@ export default {
   name: "UserSettingsView",
   data() {
     return {
-      editAppearance: false,
       bio: '',
       isEmailSubscribed: false,
       password: '',
@@ -345,9 +308,6 @@ export default {
     ...mapState("persist", ["v"])
   },
   methods: {
-    toggleAppearance() {
-      this.editAppearance = !this.editAppearance
-    },
     getEditorContent(value) {
       this.bio = value;
     }
