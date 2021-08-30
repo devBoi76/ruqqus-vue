17847f1c734568f2baed8784c87ace15e130e89a
kek7198 <grayboy99@protonmail.com>
2021-08-30 06:03:40 -0500

    styling

--- src/views/user/settings/Settings.vue
+++ src/views/user/settings/Settings.vue
@@ -22,8 +22,8 @@
               </div>
             </div>
             <div class="relative">
-              <img :src="banner" class="w-full h-56 lg:h-72 object-cover"/>
-              <div v-show="editAppearance" class="absolute bottom-0 flex items-center justify-center w-full h-full bg-black bg-opacity-30">
+              <img :src="banner" class="w-full h-56 lg:h-72 object-cover rounded-sm"/>
+              <div v-show="editAppearance" class="absolute bottom-0 flex items-center justify-center w-full h-full bg-black bg-opacity-30 rounded-sm">
                 <button type="button" class="w-9 h-9 flex items-center justify-center px-2 py-0 text-white bg-transparent hover:bg-black hover:bg-opacity-50 rounded-sm transition duration-100 ease-in-out" tabindex="0">
                   <i class="far fa-pen fa-lg"></i>
                 </button>
