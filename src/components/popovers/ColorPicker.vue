5e167bbe6f0afa15c5619ff581a4720110b3628a
kek7198 <grayboy99@protonmail.com>
2021-08-30 09:09:24 -0500

    styling

--- src/components/popovers/ColorPicker.vue
+++ src/components/popovers/ColorPicker.vue
@@ -25,7 +25,7 @@
           Custom
         </div>
         <div class="relative ml-8">
-          <input type="color" id="color" name="color" class="w-[19px] h-[24px] rounded-full absolute left-2 top-[5px]" :value="primaryColor || `#FFFFFFF`">
+          <input type="color" id="color" name="color" class="w-[18px] h-[23px] rounded-full absolute left-2 top-[6px] bg-transparent" :value="primaryColor || `#FFFFFFF`">
           <input type="text" class="form-input light text-xs pl-8" :placeholder="primaryColor || '#FFFFFFF'"/>
         </div>
       </div>
