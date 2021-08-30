bc89cae7e757767713ce95814ba498a143afbb75
kek7198 <grayboy99@protonmail.com>
2021-08-30 09:01:59 -0500

    styling

--- src/components/popovers/ColorPicker.vue
+++ src/components/popovers/ColorPicker.vue
@@ -1,7 +1,7 @@
 <template>
   <Popover v-slot="{ open }" class="relative inline-block text-left">
     <PopoverButton :class="open ? '' : 'text-opacity-90'" class="flex items-center justify-center text-white bg-red-500 group focus:outline-none hover:text-opacity-100 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 rounded-sm w-[34px] h-[34px]">
-        <i class="fas fa-fill-drip fa-fw"></i>
+      <i class="fas fa-fill-drip fa-fw"></i>
     </PopoverButton>
 
     <transition
@@ -12,16 +12,24 @@
     leave-from-class="translate-y-0 opacity-100"
     leave-to-class="translate-y-1 opacity-0"
     >
-    <PopoverPanel class="absolute right-0 origin-top-right w-48 z-10 mt-2 bg-white border rounded shadow-lg">
+    <PopoverPanel class="absolute right-0 origin-top-right w-56 z-10 mt-2 bg-white border rounded shadow-lg">
       <div class="p-3">
-        <ul class="flex flex-wrap justify-center -m-1 mb-3">
-          <li v-for="swatch in primarySwatches" :key="swatch" class="p-0.5">
+        <ul class="inline-flex flex-wrap gap-2">
+          <li v-for="swatch in primarySwatches" :key="swatch">
             <label :for="swatch" class="flex flex-shrink-0 w-5 h-5 shadow-inner rounded-sm" :style="{ 'background-color': swatch }">
               <input type="checkbox" class="invisible" :id="swatch" :value="swatch" v-model="primaryColor"/>
             </label>
           </li>
         </ul>
-        <input type="text" class="form-input light text-xs" :placeholder="primaryColor || '#FFFFFFF'"/>
+        <div class="flex items-center">
+          <div class="uppercase tracking-wide font-semibold leading-none text-2xs text-gray-500">
+            Custom
+          </div>
+          <div class="relative ml-8">
+            <input type="color" id="color" name="color" class="w-[19px] h-[24px] rounded-full absolute left-2 top-[5px]" :value="primaryColor || `#FFFFFFF`">
+            <input type="text" class="form-input light text-xs pl-8" :placeholder="primaryColor || '#FFFFFFF'"/>
+          </div>
+        </div>
       </div>
     </PopoverPanel>
   </transition>
