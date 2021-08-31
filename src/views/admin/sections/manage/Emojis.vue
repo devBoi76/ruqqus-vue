a866c57d255c031399f6c9dc77090bb36739e169
kek7198 <grayboy99@protonmail.com>
2021-08-31 18:38:59 -0500

    Update Emojis.vue

--- src/views/admin/sections/manage/Emojis.vue
+++ src/views/admin/sections/manage/Emojis.vue
@@ -61,16 +61,16 @@
 					<table class="min-w-full divide-y divide-gray-200">
 						<thead class="bg-gray-50">
 							<tr>
-								<th class="w-1/4 pl-3 pr-6 py-2 text-xs font-semibold uppercase tracking-wider text-left text-gray-400 dark:text-gray-400">
+								<th class="w-1/6 pl-3 pr-6 py-2 text-xs font-semibold uppercase tracking-wider text-left text-gray-400 dark:text-gray-400">
 									{{ emoteCount === 1 ? '1 Emote' : `${emoteCount} Emotes` }}
 								</th>
-								<th class="w-1/4 px-6 py-3 text-xs font-semibold uppercase tracking-wider text-left text-gray-400 dark:text-gray-400">
+								<th class="w-1/6 px-6 py-3 text-xs font-semibold uppercase tracking-wider text-left text-gray-400 dark:text-gray-400">
 									Uploaded by
 								</th>
-								<th class="w-1/4 px-6 py-3 text-xs font-semibold uppercase tracking-wider text-left text-gray-400 dark:text-gray-400">
+								<th class="w-3/6 px-6 py-3 text-xs font-semibold uppercase tracking-wider text-left text-gray-400 dark:text-gray-400">
 									Category
 								</th>
-								<th class="w-1/4 px-6 py-3 text-xs font-semibold uppercase tracking-wider text-left text-gray-400 dark:text-gray-400">
+								<th class="w-1/6 px-6 py-3 text-xs font-semibold uppercase tracking-wider text-left text-gray-400 dark:text-gray-400">
 									Actions
 								</th>
 							</tr>
@@ -80,9 +80,9 @@
 								<tr class="border-t first:border-0 hover:bg-gray-50">
 									<td class="pl-3 pr-6 py-3 whitespace-no-wrap">
 										<div class="flex items-center">
-											<img :src="emote.src" class="w-8 h-8 rounded-sm object-cover mr-2" :alt="`${emote.name} emote`"/>
+											<img :src="emote.src" class="w-8 h-8 rounded-sm object-cover mr-3" :alt="`${emote.name} emote`"/>
 											<div>
-												<p class="text-2xs text-gray-500">
+												<p class="text-gray-700 font-medium">
 													{{ emote.name }}
 												</p>
 											</div>
@@ -99,8 +99,8 @@
 										</span>
 									</td>
 									<td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
-										<button class="button linkPrimary">
-											Edit
+										<button class="flex items-center justify-center px-2 w-8 h-8 text-xl text-gray-600 dark:text-gray-400 hover:text-red-500 dark:hover:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white rounded-sm">
+											<i class="far fa-trash-alt"></i>
 										</button>
 									</td>
 								</tr>
