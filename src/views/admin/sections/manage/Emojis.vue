f6011080328d8cc42a47941ac41a0d1d10ccb9cc
kek7198 <grayboy99@protonmail.com>
2021-08-31 19:04:01 -0500

    icon

--- src/views/admin/sections/manage/Emojis.vue
+++ src/views/admin/sections/manage/Emojis.vue
@@ -96,7 +96,10 @@
 											{{ emote.category }}
 										</span>
 									</td>
-									<td class="px-6 py-4 whitespace-no-wrap leading-5 text-gray-600">
+									<td class="flex space-x-2 px-6 py-4 whitespace-no-wrap leading-5 text-gray-600">
+										<button class="flex items-center justify-center px-2 w-8 h-8 text-xl text-gray-600 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white rounded-sm">
+											<i class="far fa-pen"></i>
+										</button>
 										<button class="flex items-center justify-center px-2 w-8 h-8 text-xl text-gray-600 dark:text-gray-400 hover:text-red-500 dark:hover:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white rounded-sm">
 											<i class="far fa-trash-alt"></i>
 										</button>
