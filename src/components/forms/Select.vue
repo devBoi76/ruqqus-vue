d56551e8d24c7b7a76d7b6b0ed51eef9735e7a0f
kek7198 <grayboy99@protonmail.com>
2021-09-01 14:08:20 -0500

    styling

--- src/components/forms/Select.vue
+++ src/components/forms/Select.vue
@@ -5,23 +5,23 @@
       {{ selectedPerson.name }}
     </ListboxButton>
 
-    <!-- Render a `div` instead of a `ul` -->
-    <ListboxOptions as="div" class="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-      <!-- Render a `span` instead of a `li` -->
-      <ListboxOption v-slot="{ active, selected }" as="span" v-for="person in people" :key="person" :value="person">
-        <span :class="[active ? 'text-amber-900 bg-amber-100' : 'text-gray-900','cursor-default select-none relative py-2 pl-10 pr-4',]">
-          {{ person.name }}
-        </span>
-        <span v-if="selected" class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-          <i class="far fa-check fa-sm"></i>
-        </span>
-      </ListboxOption>
-      <div class="py-2 border-t">
-        <button v-show="!form" class="button linkGray400" @click="form = !form">
-          <i class="far fa-plus fa-sm pr-1"></i>
-          Create new category
-        </button>
-        <input v-show="form" type="text" placeholder="i.e. memes"/>
+    <ListboxOptions as="div" class="absolute right-0 w-64 mt-4 origin-top-right bg-white divide-y rounded shadow-lg border focus:outline-none">
+      <div>
+        <ListboxOption v-slot="{ active, selected }" as="div" v-for="person in people" :key="person" :value="person">
+          <span :class="[active ? 'text-amber-900 bg-amber-100' : 'text-gray-900','cursor-default select-none relative py-2 pl-10 pr-4',]">
+            {{ person.name }}
+          </span>
+          <span v-if="selected" class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+            <i class="far fa-check fa-sm"></i>
+          </span>
+        </ListboxOption>
+        <div class="py-2 border-t">
+          <button v-show="!form" class="button linkGray400" @click="form = !form">
+            <i class="far fa-plus fa-sm pr-1"></i>
+            Create new category
+          </button>
+          <input v-show="form" type="text" placeholder="i.e. memes"/>
+        </div>
       </div>
     </ListboxOptions>
   </Listbox>
