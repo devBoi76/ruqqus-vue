00b9cc5e01bb1e6ed74f3bf1f7f289cca93389ff
kek7198 <grayboy99@protonmail.com>
2021-09-02 12:57:12 -0500

    styling

--- src/components/modals/ModalEmoji.vue
+++ src/components/modals/ModalEmoji.vue
@@ -70,7 +70,7 @@
             </p>
           </div>
           <div class="flex justify-between mt-4 px-6 py-3 bg-gray-50 border-t">
-            <button class="flex items-center justify-center px-2 w-8 h-8 text-xl text-gray-600 dark:text-gray-400 hover:text-red-500 dark:hover:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white rounded-sm">
+            <button class="flex items-center justify-center px-2 w-8 h-8 text-red-500 dark:text-gray-400 hover:text-red-700 dark:hover:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white rounded-sm">
               <i class="far fa-trash-alt"></i>
             </button>
             <div class="flex">
