fceb8259c44b2420910fa4dd30100af010e8ae43
kek7198 <grayboy99@protonmail.com>
2021-09-22 15:13:40 -0500

    routes

--- src/views/user/Inbox.vue
+++ src/views/user/Inbox.vue
@@ -1,60 +1,54 @@
 <template>
-	<div class="flex flex-col flex-grow bg-white md:shadow-xs md:rounded-sm overflow-hidden">
-
-		<!-- Header content section -->
-		<div class="flex flex-shrink-0 w-full items-center h-16 px-4 bg-white border-b dark:border-white dark:border-opacity-10 dark:bg-gray-700">
-			<h1 class="text-xl mb-0">Notifications</h1>
-			<div class="hidden ml-auto">
-				<button class="button white" @click="getNotifications()">
-					<i class="far fa-redo-alt"></i>
-				</button>
-			</div>
-		</div>
-
-		<!-- Tabs -->
-		<div class="flex flex-shrink-0 w-full items-end h-10 px-4 border-b dark:border-white dark:border-opacity-10 dark:bg-gray-700">
-			<router-link v-for="tab in tabs" :key="tab.name" v-slot="{ href, navigate, isExactActive }" :to="tab.route">
-				<a :href="href" @click="navigate" class="group">
-					<div class="border-b-2 px-4 pb-2 capitalize" :class="isExactActive ? 'text-purple-500 border-purple-500' : 'text-gray-500 hover:text-gray-700 border-transparent'">
-						{{ tab.name }}
+	<div class="w-full overflow-y-auto">
+		<div class="grid grid-cols-12 xl:grid-cols-10">
+			<div class="col-span-full xl:col-start-3 xl:col-end-9 flex flex-col gap-4 sm:p-6 my-2.5 sm:my-0">
+				<h1 class="h3 font-semibold mb-0">
+					Notifications
+				</h1>
+				<!-- Main Content Section -->
+				<div class="border-t border-b border-gray-100 dark:border-transparent sm:border sm:border-gray-200 sm:rounded-sm bg-white">
+					<!-- Tabs -->
+					<div class="flex flex-shrink-0 w-full items-end px-4 border-b dark:border-white dark:border-opacity-10 dark:bg-gray-700">
+						<router-link v-for="tab in tabs" :key="tab.name" v-slot="{ href, navigate, isExactActive }" :to="tab.route">
+							<a :href="href" @click="navigate" class="group">
+								<div class="border-b-2 px-4 py-3 text-sm font-semibold leading-tight capitalize" :class="isExactActive ? 'text-gray-900 border-purple-500' : 'text-gray-400 hover:text-gray-500 border-transparent'">
+									{{ tab.name }}
+								</div>
+							</a>
+						</router-link>
 					</div>
-				</a>
-			</router-link>
-		</div>
-
-		<!-- Main content section -->
-		<div class="grid grid-cols-12 gap-8">
-			<div class="col-span-full dark:text-gray-100">
-				<!-- Loading state -->
-				<div v-show="loading" class="space-y-5 py-4">
-					<div v-for="i in 5" :key="i" class="flex w-full px-4 space-x-3 animate-pulse">
-						<div class="rounded-sm bg-gray-100 dark:bg-white dark:bg-opacity-20 w-8 h-8"></div>
-						<div class="rounded-sm bg-gray-100 dark:bg-white dark:bg-opacity-20 h-8 flex-grow"></div>
+					<!-- Inbox -->
+					<!-- Loading state -->
+					<div v-show="loading" class="space-y-5 py-4">
+						<div v-for="i in 5" :key="i" class="flex w-full px-4 space-x-3 animate-pulse">
+							<div class="rounded-sm bg-gray-100 dark:bg-white dark:bg-opacity-20 w-8 h-8"></div>
+							<div class="rounded-sm bg-gray-100 dark:bg-white dark:bg-opacity-20 h-8 flex-grow"></div>
+						</div>
 					</div>
-				</div>
-				<!-- Errored state -->
-				<div v-show="errored">
-					<div class="flex flex-col items-center text-center w-full p-12">
-						<i class="block fad fa-ghost text-gray-400 text-4xl mb-5"></i>
-						<div class="h5">Unable to Load Notifications</div>
-						<p class="text-sm text-gray-400">
-							Sorry, we're unable to fetch your notifications right now.
-						</p>
-						<button class="button outlineGray200 mt-3" @click="getNotifications()">
-							<i class="far fa-redo-alt pr-2"></i>
-							Try again
+					<!-- Errored state -->
+					<div v-show="errored">
+						<div class="flex flex-col items-center text-center w-full p-12">
+							<i class="block fad fa-ghost text-gray-400 text-4xl mb-5"></i>
+							<div class="h5">Unable to Load Notifications</div>
+							<p class="text-sm text-gray-400">
+								Sorry, we're unable to fetch your notifications right now.
+							</p>
+							<button class="button outlineGray200 mt-3" @click="getNotifications()">
+								<i class="far fa-redo-alt pr-2"></i>
+								Try again
+							</button>
+						</div>
+					</div>
+					<NotificationList v-if="notifications.length" :notifications="notifications"/>
+					<!-- Empty state -->
+					<div v-show="!loading && !errored" v-else class="flex flex-col items-center p-12">
+						<i class="block fad fa-bell text-gray-400 text-4xl mb-5"></i>
+						<div class="h5">No Unread Notifications</div>
+						<p class="text-sm text-gray-400">You're all caught up! Time for a coffee break? 🧙</p>
+						<button class="button outlinePurple500 mt-3">
+							View all
 						</button>
 					</div>
-				</div>
-				<NotificationList v-if="notifications.length" :notifications="notifications"/>
-				<!-- Empty state -->
-				<div v-show="!loading && !errored" v-else class="flex flex-col items-center p-12">
-					<i class="block fad fa-bell text-gray-400 text-4xl mb-5"></i>
-					<div class="h5">No Unread Notifications</div>
-					<p class="text-sm text-gray-400">You're all caught up! Time for a coffee break? 🧙</p>
-					<button class="button outlinePurple500 mt-3">
-						View all
-					</button>
 				</div>
 			</div>
 		</div>
@@ -133,17 +127,17 @@ export default {
 				icon: 'fas fa-eye-slash',
 				color: 'red-500',
 				description: 'Configure your email, password, and more.'
-			},
-			{
-				name: "system",
-				route: {
-					path: '/notifications/system'
-				},
-				exact: true,
-				icon: 'fas fa-eye-slash',
-				color: 'red-500',
-				description: 'Configure your email, password, and more.'
 			}
+			// {
+			// 	name: "system",
+			// 	route: {
+			// 		path: '/notifications/system'
+			// 	},
+			// 	exact: true,
+			// 	icon: 'fas fa-eye-slash',
+			// 	color: 'red-500',
+			// 	description: 'Configure your email, password, and more.'
+			// }
 			]
 		}
 	},
