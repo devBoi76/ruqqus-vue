ced7cff3e7eefa6e3db232c0a33d4d1fd32b883c
kek7198 <grayboy99@protonmail.com>
2021-09-27 12:02:55 -0500

    mock endpoint

--- src/api/Account.js
+++ src/api/Account.js
@@ -3,7 +3,8 @@
 
 import axios from 'axios';
 
-const url = `/account/`
+//const url = `/account/`
+const url = `https://08816b9e-9bc2-436d-96b5-621552da59d0.mock.pstmn.io/api/account/`
 
 // Return all public info given account username (or id)
 export function getAccount(id){
