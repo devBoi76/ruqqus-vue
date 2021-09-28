6143ed8311e5f12a087ae6dda3c66bbc406f8726
kek7198 <grayboy99@protonmail.com>
2021-09-28 11:09:13 -0500

    spacing

--- src/views/admin/sections/manage/Members.vue
+++ src/views/admin/sections/manage/Members.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="flex flex-col flex-grow overflow-y-auto">
+	<div class="flex flex-col flex-grow spacey-y-4 overflow-y-auto">
 
 		<!-- Header content section -->
 		<div class="grid grid-cols-12 sticky top-0 z-10">
@@ -56,12 +56,16 @@
 			<div v-if="!loading && !errored" class="col-span-full px-4 md:px-8">
 				<div class="flex items-center">
 					<div class="flex space-x-4">
-						<select class="form-select light capitalize" v-model="sort" :options="routes" >
-							<option v-for="route in routes" :value="route.id" class="capitalize">{{ route.text }}</option>
+						<select class="form-select light capitalize" v-model="sort" :options="routes">
+							<option v-for="route in routes" :value="route.id">
+								{{ route.text }}
+							</option>
 						</select>
 						<div class="flex flex-shrink-0 space-x-2">
-							<select class="form-select light" placeholder="Change role" :disabled="!selected.length" v-model="selectedRole">
-								<option v-for="role in roles" :value="role.id">{{ role.text }}</option>
+							<select class="form-select light capitalize" placeholder="Change role" :disabled="!selected.length" v-model="selectedRole">
+								<option v-for="role in roles" :value="role.id">
+									{{ role.text }}
+								</option>
 							</select>
 							<button class="button gray200" v-show="!confirmation || !selected.length" :disabled="!selectedRole || !selected.length"  @click="confirmation = true">
 								Change
