97c540119d61be7d1020ec5394f1ae335df1ecc9
kek7198 <grayboy99@protonmail.com>
2021-09-28 12:36:21 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -102,21 +102,7 @@ export default {
     }
   },
   created() {
-    const kode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-    const length = kode.length
-    var pos = 0
-
-    document.body.addEventListener('keydown', function (event) {
-      if (event.keyCode === kode[pos++]) {
-        if (length === pos) {
-          console.log('konami code activated')
-        pos = 0 // ability to start over
-        return false
-      }
-    } else {
-      pos = 0
-    }
-  }, false)
+    this.$store.dispatch('site/fetchSite')
   }
 };
 
