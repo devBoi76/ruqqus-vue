bc1d883aef712d9e16bf5244147702c50a72ac02
kek7198 <grayboy99@protonmail.com>
2021-09-28 12:21:05 -0500

    Update Banner.vue

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="relative flex flex-col items-center justify-center text-center min-h-[100px] max-h-[140px] sm:min-h-[270px] sm:max-h-[300px] z-50">
-		<img class="absolute w-full h-full bottom-0 top-0 left-0 right-0 object-cover" :src="image || site.cover" alt="banner">
+		<img class="absolute w-full h-full bottom-0 top-0 left-0 right-0 object-cover" :src="image || site.coverUrl" alt="banner">
 		<h1 class="hidden sm:block z-10 text-white mb-4">
 			{{ title || site.name }}
 		</h1>
