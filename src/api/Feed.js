4c7359510d455ca7de5fe76e7b4f850373eb7022
kek7198 <grayboy99@protonmail.com>
2021-10-06 18:57:24 -0500

    comments

--- src/api/Feed.js
+++ src/api/Feed.js
@@ -5,12 +5,20 @@ import axios from 'axios';
 
 const url = 'https://ed938631-cc32-4bb1-805e-ae9a7f5b4bca.mock.pstmn.io/api/feed/'
 
-// Return a posts of a feed. Requires a type (all, hot, etc). Optional parameters for sort, pagination, etc
+// Return a posts of a feed. For demonstration purposes.
 export function getFeed(type, params) {
-	return axios.get(url+type, {
+	return axios.get(url, {
 		params: params
 	});
 }
+
+// Return a posts of a feed. Requires a type (all, hot, etc). Optional parameters for sort, pagination, etc
+// export function getFeed(type, params) {
+// 	return axios.get(url+type, {
+// 		params: params
+// 	});
+// }
+
 
 
 
