4c017f4b39efc17d98ee397964a67e6024d679d0
kek7198 <grayboy99@protonmail.com>
2021-10-06 19:20:12 -0500

    Update items.js

--- src/store/modules/items.js
+++ src/store/modules/items.js
@@ -203,8 +203,8 @@ const actions = {
 		console.log(payload.params)
 		getFeed(payload.feed, payload.params)
 		.then(response => {
-			commit("SET_ITEMS", response.data.results);
 			console.log(response.data)
+			commit("SET_ITEMS", response.data.results);
 			//commit('guild/SET_GUILDS', response.data.results, { root: true });
 		})
 	},
