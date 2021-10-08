0f2285c39de432c50494e0c826a80e0628326c1a
kek7198 <grayboy99@protonmail.com>
2021-10-08 12:11:40 -0500

    test transition

--- src/components/notifications/ToastList.vue
+++ src/components/notifications/ToastList.vue
@@ -1,6 +1,14 @@
 <template>
 	<div class="fixed mx-auto bottom-4 md:top-18 md:right-4 space-y-3 w-80 z-100">
-		<Notification v-for="notification in notifications" :key="notification.id" :notification="notification"/>
+		<transition-group name="toasts"
+          enter="duration-300 ease-out"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="duration-200 ease-in"
+          leave-from="opacity-100"
+          leave-to="opacity-0">
+			<Notification v-for="notification in notifications" :key="notification.id" :notification="notification"/>
+		<transition-group/>
 	</div>
 </template>
 
