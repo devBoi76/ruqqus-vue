a5f740d61509b0979f8068b5b109ad0e536ad171
kek7198 <grayboy99@protonmail.com>
2021-10-08 11:51:37 -0500

    styling

--- src/components/notifications/Toast.vue
+++ src/components/notifications/Toast.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="flex justify-between px-3 py-2 text-sm rounded-sm shadow bg-white transition-all duration-200">
-    <div class="flex space-x-4">
-      <i class='far fa-lg' :class="notification.type === 'success' ? 'fa-check-circle text-green-500' : 'fa-exclamation-circle text-red-500'"></i>
+  <div class="flex justify-between px-4 py-3 rounded shadow-lg bg-gray-900 transition-all duration-200">
+    <div class="flex space-x-4 pr-4">
+      <i class='far fa-lg mt-2' :class="notification.type === 'success' ? 'fa-check-circle text-green-500' : 'fa-exclamation-circle text-red-500'"></i>
       <div>
-        <div v-if="notification.header" class="font-medium">
+        <div v-if="notification.header" class="text-base font-medium">
           {{ notification.header }}
         </div>
-        <div class="text-gray-600">
+        <div class="text-sm text-gray-200">
           {{ notification.message }}
         </div>
       </div>
     </div>
-    <button class="w-8 h-8 flex items-center justify-center px-2 py-0 text-gray-500 rounded-sm bg-white hover:bg-gray-200" aria-label="Close toast" @click="removeNotification(notification)">
+    <button class="w-8 h-8 flex items-center justify-center px-2 py-0 text-gray-500 rounded-sm bg-gray-900 hover:bg-gray-800" aria-label="Close toast" @click="removeNotification(notification)">
       <i class="far fa-times"></i>
     </button>
   </div>
