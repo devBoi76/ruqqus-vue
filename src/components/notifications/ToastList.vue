5badf746b71fd12167d954cded193a8fc240c6c1
kek7198 <grayboy99@protonmail.com>
2021-10-08 11:55:05 -0500

    styling

--- src/components/notifications/ToastList.vue
+++ src/components/notifications/ToastList.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="fixed mx-auto bottom-4 md:top-18 md:right-4 w-full space-y-3 z-50">
+	<div class="fixed mx-auto bottom-4 md:top-18 md:right-4 space-y-3 z-50">
 		<Notification v-for="notification in notifications" :key="notification.id" :notification="notification"/>
 	</div>
 </template>
