1b027b9a4e54e5a42277a59d637de07c70b357f0
kek7198 <grayboy99@protonmail.com>
2021-10-08 11:54:39 -0500

    Update Toast.vue

--- src/components/notifications/Toast.vue
+++ src/components/notifications/Toast.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex justify-between px-4 py-3 rounded shadow-lg bg-gray-900 text-white transition-all duration-200">
-    <div class="flex space-x-4 pr-4">
+  <div class="flex justify-between p-3 rounded shadow-lg bg-gray-900 text-white transition-all duration-200">
+    <div class="flex space-x-4 pr-3">
       <i class='far fa-lg mt-2' :class="notification.type === 'success' ? 'fa-check-circle text-green-500' : 'fa-exclamation-circle text-red-500'"></i>
       <div>
         <div v-if="notification.header" class="text-base font-medium">
