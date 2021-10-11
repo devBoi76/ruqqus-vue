864fa0ec02fb69768cffa61127c4770e16f26a20
kek7198 <grayboy99@protonmail.com>
2021-10-11 16:23:25 -0500

    test

--- src/components/notifications/ToastList.vue
+++ src/components/notifications/ToastList.vue
@@ -1,14 +1,16 @@
 <template>
-	<div class="fixed bottom-4 md:top-20 md:right-4 space-y-3 w-auto z-100">
-		<transition-group name="toasts"
-          enter-active-class="transition-all duration-200 ease-in"
-          enter-from-class="opacity-0 translate-x-10"
-          enter-to-class="opacity-100"
-          leave-active-class="transition-all duration-200 ease-out"
-          leave-from-class="opacity-100"
-          leave-to-class="opacity-0 translate-x-10">
-			<Notification v-for="notification in notifications" :key="notification.id" :notification="notification" class="mx-auto"/>
-		</transition-group>
+	<div class="fixed bottom-4">
+		<div class="mx-auto space-y-3 w-auto z-100">
+			<transition-group name="toasts"
+			enter-active-class="transition-all duration-200 ease-in"
+			enter-from-class="opacity-0 translate-x-10"
+			enter-to-class="opacity-100"
+			leave-active-class="transition-all duration-200 ease-out"
+			leave-from-class="opacity-100"
+			leave-to-class="opacity-0 translate-x-10">
+				<Notification v-for="notification in notifications" :key="notification.id" :notification="notification" class="mx-auto"/>
+			</transition-group>
+		</div>
 	</div>
 </template>
 
