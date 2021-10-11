e925ae13f59a22d75b782d4f00b191cc8e92b229
kek7198 <grayboy99@protonmail.com>
2021-10-11 16:37:28 -0500

    Update ToastList.vue

--- src/components/notifications/ToastList.vue
+++ src/components/notifications/ToastList.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="fixed bottom-4 w-full z-100">
-		<div class="mx-auto w-96 space-y-3">
+	<div class="fixed bottom-4 w-full flex items-center z-100">
+		<div class="w-96 space-y-3">
 			<transition-group name="toasts"
 			enter-active-class="transition-all duration-200 ease-in"
 			enter-from-class="opacity-0 translate-y-10"
