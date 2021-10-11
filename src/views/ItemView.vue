d55813d57fb0415d5d5f97f9f027c58b0517308d
kek7198 <grayboy99@protonmail.com>
2021-10-11 16:01:42 -0500

    Update ItemView.vue

--- src/views/ItemView.vue
+++ src/views/ItemView.vue
@@ -21,7 +21,7 @@
 					<!-- End Bread Crumbs -->
 
 					<!-- Pinned Banner -->
-					<div v-if="item && item.isStickied" class="flex items-center mb-3 px-4 py-2 bg-green-100 text-green-700 border border-green-200 rounded-sm">
+					<div v-if="item && item.isStickied" class="flex items-center mb-3 px-4 py-2 bg-green-100 text-green-700 border border-green-100 rounded-sm">
 						<i class="fas fa-thumbtack fa-fw fa-sm mr-2"></i>
 						<span>This post has been pinned by the admins.</span>
 					</div>
